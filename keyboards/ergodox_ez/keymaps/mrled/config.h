27a8e824479edf55818049174714e674936e39dd
Micah R Ledbetter <me@micahrl.com>
2019-12-19 09:16:52 -0600

    Add new tap dances for playing around

--- keyboards/ergodox_ez/keymaps/mrled/config.h
+++ keyboards/ergodox_ez/keymaps/mrled/config.h
@@ -4,3 +4,4 @@
 */
 #define ORYX_CONFIGURATOR
 #define TAPPING_TERM 200
+/* #define PERMISSIVE_HOLD */
--- keyboards/ergodox_ez/keymaps/mrled/keymap.c
+++ keyboards/ergodox_ez/keymaps/mrled/keymap.c
@@ -38,8 +38,29 @@ enum custom_keycodes {
   HSV_229_102_255,
 };
 
+/* List of my tap dances */
 enum {
  DANCE_COLON = 0,
+ DANCE_CTRLX,
+ DANCE_LGUICURLY,
+ DANCE_RGUICURLY,
+};
+
+/* Used in the cur_dance function to hold state */
+typedef struct {
+  bool is_press_action;
+  int state;
+} dancestep;
+
+/* List of all the types of taps/holds we know about as dance steps */
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5, //send two single taps
+  TRIPLE_TAP = 6,
+  TRIPLE_HOLD = 7,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -274,7 +295,151 @@ void dance_colon_reset (qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
+/* Enable multi function tap dance
+ * See also <https://docs.qmk.fm/#/feature_tap_dance?id=example-4-39quad-function-tap-dance39>
+ */
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+int current_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    /*
+     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+     * keystrokes of the key, and not the 'double tap' action/macro.
+    */
+    if (state->interrupted) return DOUBLE_SINGLE_TAP;
+    else if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  //Assumes no one is trying to type the same letter three times (at least not quickly).
+  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+  if (state->count == 3) {
+    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }
+  else return 8; //magic number. At some point this method will expand to work for more presses
+}
+
+/* Similar to the previous one, but designed just for space cadet keys
+ * This supports either a tap or a hold; double taps, tap-holds etc are not interpretted specially
+ */
+int spacecadet_dance(qk_tap_dance_state_t *state) {
+  if (state->interrupted) return SINGLE_HOLD;
+  else if (!state->pressed) return SINGLE_TAP;
+  else return SINGLE_HOLD;
+}
+
+
+// dancestep objects for each of the tap dances that require them
+static dancestep dance_lguicurly = { .is_press_action=true, .state=0 };
+static dancestep dance_rguicurly = { .is_press_action=true, .state=0 };
+static dancestep dance_ctrlx = { .is_press_action=true, .state=0 };
+
+/* The ctrl-x dance
+ * See also https://docs.qmk.fm/#/feature_tap_dance?id=example-4-39quad-function-tap-dance39
+ * Allow one key to have 4 (or more) functions, in this case:
+ *   Tap = Send x
+ *   Hold = Send Control
+ *   Double Tap = Send Escape
+ *   Double Tap and Hold = Send Alt
+ */
+void dance_ctrlx_finished(qk_tap_dance_state_t *state, void *user_data) {
+  dance_ctrlx.state = current_dance(state);
+  switch (dance_ctrlx.state) {
+  case SINGLE_TAP: register_code(KC_X); break;
+  case SINGLE_HOLD: register_code(KC_LCTRL); break;
+  case DOUBLE_TAP: register_code(KC_ESC); break;
+  case DOUBLE_HOLD: register_code(KC_LALT); break;
+  case DOUBLE_SINGLE_TAP: register_code(KC_X); unregister_code(KC_X); register_code(KC_X); break;
+  //Last case is for fast typing. Assuming your key is `f`:
+  //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+  //In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+  }
+}
+void dance_ctrlx_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (dance_ctrlx.state) {
+  case SINGLE_TAP: unregister_code(KC_X); break;
+  case SINGLE_HOLD: unregister_code(KC_LCTRL); break;
+  case DOUBLE_TAP: unregister_code(KC_ESC); break;
+  case DOUBLE_HOLD: unregister_code(KC_LALT); break;
+  case DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
+  }
+  dance_ctrlx.state = 0;
+}
+
+
+/* The lguicurly dance
+ * Tap the LGUI key for an open curly brace ({); hold it for LGUI
+ */
+void dance_lguicurly_finished(qk_tap_dance_state_t *state, void *user_data) {
+  dance_lguicurly.state = current_dance(state);
+  switch (dance_lguicurly.state) {
+  case SINGLE_TAP: register_code16(S(KC_LBRACKET)); break;
+  case SINGLE_HOLD: register_mods(MOD_BIT(KC_LGUI)); break;
+  case DOUBLE_TAP: tap_code16(S(KC_LBRACKET)); register_code16(S(KC_LBRACKET)); break;
+  }
+}
+void dance_lguicurly_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (dance_lguicurly.state) {
+  case SINGLE_TAP: unregister_code16(S(KC_LBRACKET)); break;
+  case SINGLE_HOLD: unregister_mods(MOD_BIT(KC_LGUI)); break;
+  case DOUBLE_TAP: unregister_code16(S(KC_LBRACKET)); break;
+  }
+}
+
+/* The rguicurly dance
+ * Tap the LGUI key for an open curly brace ({); hold it for LGUI
+ */
+void dance_rguicurly_finished(qk_tap_dance_state_t *state, void *user_data) {
+  dance_rguicurly.state = current_dance(state);
+  switch (dance_rguicurly.state) {
+  case SINGLE_TAP: register_code(KC_RSFT); register_code(KC_RBRACKET); break;
+  case SINGLE_HOLD: register_code(KC_RGUI); break;
+  }
+}
+void dance_rguicurly_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (dance_rguicurly.state) {
+  case SINGLE_TAP: unregister_code(KC_RSFT); unregister_code(KC_RBRACKET); break;
+  case SINGLE_HOLD: unregister_code(KC_RGUI); break;
+  }
+}
+
+
 //All tap dance functions would go here. Only showing this one.
 qk_tap_dance_action_t tap_dance_actions[] = {
- [DANCE_COLON] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_colon_finished, dance_colon_reset)
+    [DANCE_COLON] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_colon_finished, dance_colon_reset),
+    [DANCE_CTRLX] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_ctrlx_finished, dance_ctrlx_reset),
+    [DANCE_LGUICURLY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_lguicurly_finished, dance_lguicurly_reset),
+    [DANCE_RGUICURLY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_rguicurly_finished, dance_rguicurly_reset),
 };
